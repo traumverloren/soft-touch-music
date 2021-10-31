f95d76fc3c2f403f715a1e9d9d5256dfb820ce8b
Stephanie Nemeth <traumverloren@gmail.com>
2021-10-31 13:02:57 +0100

    whatever

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ wire.scan(function(err, data) {
 
 wire.on('data', function(data) {
   // result for continuous stream contains data buffer, address, length, timestamp
-  console.log(data)
+  console.log(data.data)
 });
 
-wire.stream(0, 0x20, 500); // continuous stream, delay in ms
+// choose 32 as length cuz it was highest # that didn't error
+// choose 0 as command cuz not sending a command, just want to read and arbitarily chose 0
+wire.stream(0, 32, 500); // setup i2c stream: command = 0???, length=32???, delay=500ms
 
